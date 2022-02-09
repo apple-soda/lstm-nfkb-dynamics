4ddedac4b2092065cc55216529fe8c5c8071e4f8
kekpy <kekpyds@gmail.com>
2022-02-09 15:23:23 -0800

    updated and optimized model

--- core/.ipynb_checkpoints/network-checkpoint.py
+++ core/.ipynb_checkpoints/network-checkpoint.py
@@ -14,9 +14,9 @@ class LSTM(nn.Module):
         self.num_layers = num_layers
         self.device = torch.device(device)
 
-        self.lstm = nn.LSTM(input_size, hidden_sizes[0], batch_first=True) # 1, 9
-        self.fc1 = nn.Linear(hidden_sizes[0], hidden_sizes[1])
-        self.fc2 = nn.Linear(hidden_sizes[1], output_size) # 9, 9, change last parameter to 1 or 9 depending on multi-hot encoding
+        self.lstm = nn.LSTM(input_size, hidden_sizes, batch_first=True, num_layers=num_layers) # 1, 9
+        self.fc1 = nn.Linear(hidden_sizes, output_size)
+        # self.fc2 = nn.Linear(hidden_sizes[1], output_size) # 9, 9, change last parameter to 1 or 9 depending on multi-hot encoding
         
         self.to(self.device) 
         
@@ -24,23 +24,27 @@ class LSTM(nn.Module):
         # import pdb; pdb.set_trace()
         # input must be (N,L,H) because batch first : (64, 98, 1)
         # Initializing hidden state and cell state for first input with zeros 
-        h0 = torch.zeros(x.shape[2], x.shape[0], self.hidden_sizes[0], dtype=torch.float32).requires_grad_() # num layers, B (batch size), H1 (hidden size)
-        c0 = torch.zeros(x.shape[2], x.shape[0], self.hidden_sizes[0], dtype=torch.float32).requires_grad_()
+        h0 = torch.zeros(self.num_layers, x.shape[0], self.hidden_sizes, dtype=torch.float32).requires_grad_() # num layers, B (batch size), H1 (hidden size)
+        c0 = torch.zeros(self.num_layers, x.shape[0], self.hidden_sizes, dtype=torch.float32).requires_grad_()
         h0 = h0.to(self.device)
         c0 = c0.to(self.device)
         # We need to detach as we are doing truncated backpropagation through time (BPTT)
         # If we don't, we'll backprop all the way to the start even after going through another batch
         # Forward propagation by passing in the input, hidden state, and cell state into the model
         out, (hn, cn) = self.lstm(x, (h0.detach(), c0.detach()))
-
+        
+        
         # Reshaping the outputs in the shape of (batch_size, seq_length, hidden_size)
         # so that it can fit into the fully connected layer
         
-        out = out[:, -1, :]
-
-        # Convert the final state to our desired output shape (batch_size, output_dim)
-        out = self.fc1(out)
-        out = self.fc2(out)
+        # N = 1 LAYERS
+        # hn = hn.view(-1, self.hidden_sizes) # single layer
+        # out = self.fc1(hn) # single layer
+        
+        # N > 1 LAYERS
+        hn = hn.view(self.num_layers, x.size(0), self.hidden_sizes)[-1]
+        out = self.fc1(hn)
         
         return out
         
+        #out = self.fc2(out)
--- core/utils.py
+++ core/utils.py
@@ -7,3 +7,18 @@ import numpy as np
 def to_categorical(y, num_classes):
     """ 1-hot encodes a tensor """
     return np.eye(num_classes, dtype='uint8')[y]
+
+# dataloader_val = torch.utils.data.DataLoader(dataset_val, batch_size=32, shuffle=True, num_workers=4)
+# dic = {"y_pred": np.array([]), "y_true": np.array([])}
+# for x_batch, y_batch in dataloader_val:
+#     x_batch, y_batch = x_batch.to(self.device), y_batch.to(self.device)
+#     # to do : convert y_batch to np array and vstack it y_true
+#     y_batch = y_batch.detach().cpu().numpy()
+#     dic[y_true].vstack(y_true, y_batch)
+#     y_pred = trainer.model(x_batch)
+#     y_pred = F.softmax(y_pred, dim=1)
+#     # to do : convert to np array and vstack it to y_pred
+#     y_pred = y_pred.detach().cpu().numpy()
+#     dic[y_pred].vstack(y_pred)
+    
+# df = pd.DataFrame(dic)
