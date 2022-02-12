84320ef5dc0d92ee744a626cca9d8b30b340af4b
kekpy <kekpyds@gmail.com>
2022-02-12 00:24:41 -0800

    updated reports

--- core/trainer.py
+++ core/trainer.py
@@ -3,30 +3,42 @@ import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
 import tqdm 
+import pickle
 import numpy as np
 import sklearn.metrics
 
 class LSTMTrainer:
     def __init__(self, model, lr=1e-3, device="cpu"):
-        self.optimizer = optim.Adam(model.parameters(), lr=lr)
+        self.optim = optim.Adam(model.parameters(), lr=lr)
         self.device = torch.device(device)
-        self.model = model
+        self.network = model
         self.loss_fn = nn.CrossEntropyLoss().to(self.device)
-        self.train_losses = []
-        self.val_losses = []
+        self.loss_history = [[],[]]
     
     def train_step(self, x, y):
-        self.optimizer.zero_grad()
-        y_pred = self.model(x)
+        self.optim.zero_grad()
+        y_pred = self.network(x)
         loss = self.loss_fn(y_pred, y) 
         loss.backward()
-        self.optimizer.step()
+        self.optim.step()
         return loss
     
     def validate_step(self, x, y):
-        y_pred = self.model(x)
+        y_pred = self.network(x)
         loss = self.loss_fn(y_pred, y) 
         return loss
+    
+    def load(self, path, map_location=None):
+        checkpoint = torch.load(path, map_location=map_location)
+        self.network.load_state_dict(checkpoint['network'])
+        self.optim.load_state_dict(checkpoint['optim'])
+    
+    def save(self, path): #fix all this code later lol
+        torch.save({
+            'network': self.network.state_dict(),
+            'optim': self.optim.state_dict(),
+            'loss_history': self.loss_history,
+        }, path)
     
     def train(self, dataloader_train, dataloader_val, batch_size=64, n_epochs=50):
         for epoch in tqdm.tqdm(range(1, n_epochs + 1)):
@@ -37,7 +49,7 @@ class LSTMTrainer:
                 loss = float(loss.cpu().detach())
                 batch_losses.append(loss)
             epoch_loss = np.mean(batch_losses)
-            self.train_losses.append(epoch_loss)  
+            self.loss_history[0].append(epoch_loss)  
         
             val_losses = []
             for x_batch, y_batch in dataloader_val:
@@ -46,7 +58,7 @@ class LSTMTrainer:
                 loss = float(loss.cpu().detach())
                 val_losses.append(loss)
             vpoch_loss = np.mean(val_losses)
-            self.val_losses.append(vpoch_loss)
+            self.loss_history[1].append(vpoch_loss)
             
             print(f'Epoch {epoch+0:03}: | Training Loss: {epoch_loss} | Validation Loss: {vpoch_loss}')
         
