290b227ae5d8fb2d639c1ceccf86d659bf751731
kekpy <kekpyds@gmail.com>
2022-02-27 04:22:03 -0800

    kfcv

--- core/trainer.py
+++ core/trainer.py
@@ -17,8 +17,11 @@ class LSTMTrainer:
         self.network = model
         self.loss_fn = nn.CrossEntropyLoss().to(self.device)
         self.loss_history = [[],[]]
-        self.kfcv_histories = []
-    
+        
+        """
+        Probabilities, y_pred, y_true, etc initalized in respective functions
+        """
+        
     def train_step(self, x, y):
         self.optim.zero_grad()
         y_pred = self.network(x)
@@ -71,18 +74,47 @@ class LSTMTrainer:
                 self.loss_history[1].append(vpoch_loss)
 
                 print(f'Epoch {epoch+0:03}: | Training Loss: {epoch_loss} | Validation Loss: {vpoch_loss}')
+                
+    def evaluate(self, dataloader_val, batch_size=64, probability_size=9):
+      
+        self.y_prob = np.empty([batch_size, probability_size])
+        self.y_pred = np.empty([batch_size, ])
+        self.y_true = np.empty([batch_size, ])
+        
+        for x, y in dataloader_val:
+            x, y = x.to(self.device), y.to(self.device)
+            y_true = y.detach().cpu().numpy()
+            self.y_true = np.hstack([self.y_true, y_true.T])
+            y_pred = self.network(x)
+            y_pred = F.softmax(y_pred, dim=1)
+            self.y_pred = y_pred.detach().cpu().numpy()
+            self.y_prob = np.vstack([self.y_prob, y_pred])
+            y_pred = np.argmax(y_pred, axis=1)
+            self.y_prob = np.hstack([self.y_prob, y_pred.T])
     
+        self.y_prob = self.y_prob[batch_size:]
+        self.y_pred = self.y_pred[batch_size:]
+        self.y_true = self.y_true[batch_size:]
+        
+        return self.y_prob, self.y_pred, self.y_true
+        
     def kfcv(self, dataset, k, save_dir, batch_size=65, n_epochs=50):
         """
         Function for k-fold cross validation.
         Length of k_loss must equal k, returns a list of the loss history instead of storing it in self.
         Only optimized for 5-fold cross validation as of right now since I need to implement some algorithm to set the batch sizes and split the dataset accordingly.
         """
-        
+        self.kfcv_histories = []
+        self.kfcv_prob = []
+        self.kfcv_pred = []
+        self.kfcv_true = []
         d1, d2, d3, d4, d5, remainder = torch.utils.data.random_split(dataset, [13910, 13910, 13910, 13910, 13910, 2]) #remainder can be discarded
+        
         for i in range(k):
             self.kfcv_histories.append([])
-        
+            self.kfcv_prob.append([])
+            self.kfcv_pred.append([])
+            self.kfcv_true.append([])
         """
         CV 1
         """
@@ -93,6 +125,7 @@ class LSTMTrainer:
         dataloader_val = torch.utils.data.DataLoader(dataset_val, batch_size=65, shuffle=True)
         self.train(dataloader_train, dataloader_val, batch_size=batch_size, n_epochs=n_epochs)
         self.kfcv_histories[0] = self.loss_history
+        self.kfcv_prob[0], self.kfcv_pred[0], self.kfcv_true[0] = self.evaluate(dataloader_val, batch_size=65, probability_size=9)
         self.loss_history = [[], []] 
         self.save('../models/cfv/' + save_dir + '1' + '.pth')
         """
@@ -105,6 +138,7 @@ class LSTMTrainer:
         dataloader_val = torch.utils.data.DataLoader(dataset_val, batch_size=65, shuffle=True)
         self.train(dataloader_train, dataloader_val, batch_size=batch_size, n_epochs=n_epochs)
         self.kfcv_histories[1] = self.loss_history
+        self.kfcv_prob[1], self.kfcv_pred[1], self.kfcv_true[1] = self.evaluate(dataloader_val, batch_size=65, probability_size=9)
         self.loss_history = [[], []] 
         self.save('../models/cfv/' + save_dir + '2' + '.pth')
         """
@@ -117,6 +151,7 @@ class LSTMTrainer:
         dataloader_val = torch.utils.data.DataLoader(dataset_val, batch_size=65, shuffle=True)
         self.train(dataloader_train, dataloader_val, batch_size=batch_size, n_epochs=n_epochs)
         self.kfcv_histories[2] = self.loss_history
+        self.kfcv_prob[2], self.kfcv_pred[2], self.kfcv_true[2] = self.evaluate(dataloader_val, batch_size=65, probability_size=9)
         self.loss_history = [[], []] 
         self.save('../models/cfv/' + save_dir + '3' + '.pth')
         """
@@ -129,6 +164,7 @@ class LSTMTrainer:
         dataloader_val = torch.utils.data.DataLoader(dataset_val, batch_size=65, shuffle=True)
         self.train(dataloader_train, dataloader_val, batch_size=batch_size, n_epochs=n_epochs)
         self.kfcv_histories[3] = self.loss_history
+        self.kfcv_prob[3], self.kfcv_pred[3], self.kfcv_true[3] = self.evaluate(dataloader_val, batch_size=65, probability_size=9)
         self.loss_history = [[], []] 
         self.save('../models/cfv/' + save_dir + '4' + '.pth')
         """
@@ -141,6 +177,7 @@ class LSTMTrainer:
         dataloader_val = torch.utils.data.DataLoader(dataset_val, batch_size=65, shuffle=True)
         self.train(dataloader_train, dataloader_val, batch_size=batch_size, n_epochs=n_epochs)
         self.kfcv_histories[4] = self.loss_history
+        self.kfcv_prob[4], self.kfcv_pred[4], self.kfcv_true[4] = self.evaluate(dataloader_val, batch_size=65, probability_size=9)
         self.loss_history = [[], []] 
         self.save('../models/cfv/' + save_dir + '5' + '.pth')
         
